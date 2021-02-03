460daac7c74b45782c4096c694f008d505684734
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-03 23:02:56 +0000

    C program that prints a message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /**
  * main - Entry point
+ *
+ * Description: Prints a message with "puts"
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
