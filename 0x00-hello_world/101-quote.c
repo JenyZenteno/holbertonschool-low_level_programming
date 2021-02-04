9e93ba18d8354a3254a8ae2766b9c33ef3413f82
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-04 12:57:16 +0000

    C program that prints a message with write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - Entry point
  *
- * Description: Prints a frase of Dora korpar with "write"
+ * Description: Prints a frase of Dora k. with "write"
  * Return: Always 1 (Success)
  */
 int main(void)
 {
- write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
- return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
