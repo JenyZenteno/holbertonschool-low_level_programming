936bb6ef5d6569970b7aac2492bbcd1eb0dce1f4
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-05 18:06:42 +0000

    Print unique combinations of 4 digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: Print unique combinations 4 digits
+ * Description: Print unique combinations of 0-99
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,20 +11,20 @@ int d1;
 int d2;
 int d3;
 int d4;
-for (d1 = '0'; d1 <= '9'; d1++) /*print first two digit combo*/
+for (d1 = 48; d1 <= 57; d1++)
 {
-for (d2 = '0'; d2 <= '9'; d2++)
+for (d2 = 48; d2 <= 57; d2++)
 {
-for (d3 = d1 ; d3 <= '9'; d3++) /*print second of pair*/
+for (d3 = d1 ; d3 <= 57; d3++)
 {
-for (d4 = d2 + 1; d4 <= '9'; d4++)
+for (d4 = d2 + 1; d4 <= 57; d4++)
 {
 putchar(d1);
 putchar(d2);
 putchar(' ');
 putchar(d3);
 putchar(d4);
-if (!((d1 == '9' && d2 == '8') && (d3 == '9' && d4 == '9')))
+if (!((d1 == 57 && d2 == 56) && (d3 == 57 && d4 == 57)))
 {
 putchar(',');
 putchar(' ');
