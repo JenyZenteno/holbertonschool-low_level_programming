2779678594ec4bbf42b68e2f3ea9a6a2db4a7965
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-05 00:23:49 +0000

    prints the 9 first numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the 9 first numbers with putchar()
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
