2c1a1bd97e33a050e1b785bb7ac1e43916ec5654
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-06 14:52:12 +0000

    Prints the combinations of 4 digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: Print unique combinations of 0-99
+ * Description: Print unique combinations of 4 digits
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -13,23 +13,26 @@ int d3;
 int d4;
 for (d1 = 48; d1 <= 57; d1++)
 {
-for (d2 = 48; d2 <= 57; d2++)
+for (d2 = 48; d2 <= 56; d2++)
 {
+d4 = d2 + 1;
 for (d3 = d1 ; d3 <= 57; d3++)
 {
-for (d4 = d2 + 1; d4 <= 57; d4++)
+while (d4 <= 57)
 {
 putchar(d1);
 putchar(d2);
 putchar(32);
 putchar(d3);
 putchar(d4);
-if (!((d1 == 57 && d2 == 56) && (d3 == 57 && d4 == 57)))
+if (!(d2 == 56 && d4 == 57))
 {
 putchar(44);
 putchar(32);
 }
+d4++;
 }
+d4 = 48;
 }
 }
 }
