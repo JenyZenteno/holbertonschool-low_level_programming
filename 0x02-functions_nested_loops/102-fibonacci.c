5f0d8fac0752714ae40988db7416c851ef8ea250
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-10 02:51:08 +0000

    Fibonacci number

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,8 +8,8 @@
 int main(void)
 {
 	int counter;
-       	unsigned long a = 0;
-	unsigned long b = 2;
+	unsigned long a = 0;
+	unsigned long b = 1;
 	unsigned long c;
 
 	for (counter = 1; counter < 50; counter++)
