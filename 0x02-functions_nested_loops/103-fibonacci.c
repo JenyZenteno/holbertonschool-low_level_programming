5285f3108a540a0b7cd789a981d2ab6476deaa88
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-10 03:16:53 +0000

    Fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - print first 50 Fibonacci numbers
+ * main - print the sum of Fibonacci numbers
  * Return: 0
  */
 
 int main(void)
 {
-	int counter;
 	unsigned long a = 0;
 	unsigned long b = 1;
 	unsigned long c = 0;
 	unsigned long sum = 0;
 
-	while (c < 400000)
+	while (c < 4000000)
 	{
 		c = a + b;
 		if (c % 2 == 0)
