839beccb657e547a2f118ed0aa3b6947e8088f08
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-17 16:15:59 +0000

    Test

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,26 +10,27 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int accumulator;
+	int accumulator = '0';
 	int length = _strlen(s);
 	int initial_possition = 0;
 	int final_possition = 0;
 
-	while (length > 0)
+	while (i < length)
 	{
-		if ((s[i] >= 48 && s[i] >= 57) && s[i + 1] != 32)
+		if ((s[i] >= 48 && s[i] <= 57))
 		{
-			accumulator = (s[i] + '0');
-			if (initial_possition == 0)
+			accumulator += (s[i] + '0');
+			if (initial_possition == 0 && length > 1)
 				initial_possition = i;
 			final_possition = i;
 		}
+		if ((final_possition > 0) && !(s[i] >= 48 && s[i] <= 57))
+			break;
+		i++;
 	}
 
 	if (initial_possition > 1)
-		if ((s[initial_possition - 1] == 45)
-		    && (s[final_possition + 1] != 32))
+		if (s[initial_possition - 1] == 45)
 			accumulator = '-' + accumulator;
-
 	return (accumulator);
 }
