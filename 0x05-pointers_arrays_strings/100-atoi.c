adadbe1de11de3ce9ae77ba63cfab8e20530797d
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-17 23:26:03 +0000

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,23 @@
 #include "holberton.h"
-#include "2-strlen.c"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
 
 /**
  * _atoi - returns the numbers of a string
@@ -10,27 +28,27 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int accumulator = '0';
-	int length = _strlen(s);
+	int accumulator = 0;
+	int length = _strlen(s) - 1;
 	int initial_possition = 0;
 	int final_possition = 0;
 
-	while (i < length)
+	while (i <= length)
 	{
-		if ((s[i] >= 48 && s[i] <= 57))
+		if ((s[i] >= '0' && s[i] <= '9'))
 		{
-			accumulator += (s[i] + '0');
+			accumulator = (accumulator * 10) + (s[i] - '0');
 			if (initial_possition == 0 && length > 1)
 				initial_possition = i;
 			final_possition = i;
 		}
-		if ((final_possition > 0) && !(s[i] >= 48 && s[i] <= 57))
+		if ((final_possition > 0) && !(s[i] >= '0' && s[i] <= '9'))
 			break;
 		i++;
 	}
-
-	if (initial_possition > 1)
-		if (s[initial_possition - 1] == 45)
-			accumulator = '-' + accumulator;
+	if (initial_possition >= 1)
+		if ((s[--initial_possition] == '-' && accumulator != 0)
+		    && !(s[++final_possition] == ' '))
+			accumulator *= -1;
 	return (accumulator);
 }
