93683913a04fe761f11bbb48a747bdc0e0ca80f8
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-17 03:48:32 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include "2-strlen.c"
+
+/**
+ * _atoi - returns the numbers of a string
+ * @s: string
+ * Return: accumulator
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int accumulator;
+	int length = _strlen(s);
+	int initial_possition = 0;
+	int final_possition = 0;
+
+	while (length > 0)
+	{
+		if ((s[i] >= 48 && s[i] >= 57) && s[i + 1] != 32)
+		{
+			accumulator = (s[i] + '0');
+			if (initial_possition == 0)
+				initial_possition = i;
+			final_possition = i;
+		}
+	}
+
+	if (initial_possition > 1)
+		if ((s[initial_possition - 1] == 45)
+		    && (s[final_possition + 1] != 32))
+			accumulator = '-' + accumulator;
+
+	return (accumulator);
+}
