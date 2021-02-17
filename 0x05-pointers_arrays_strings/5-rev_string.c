8fb6e84bcb69c8722872108b6432bbfb87b77b0f
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-17 03:19:05 +0000

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,7 @@
 #include "holberton.h"
+#include "2-strlen.c"
 
-/**
- * _length - returns the length of a string
- * @s: string
- * Return: length
- */
-
-int _strlen(char *s)
-{
-	int length =  0;
-
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-	return (length);
-}
-
-/**
+ /**
  * rev_string - reverses a string
  * @s: string to reverse
  */
