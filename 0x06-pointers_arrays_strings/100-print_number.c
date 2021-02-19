4f111e2f19dd687f56afcfd740918a6d40e38a55
JenyZenteno <zentenocastanedajeny@gmail.com>
2021-02-19 01:53:56 +0000

    Pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_number - print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int tens, digit, positive = n;
+	double tens_increase = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
+
+		while (tens_increase <= positive)
+			tens_increase *= 10;
+		tens = tens_increase / 10;
+
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
+	}
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * *string_toupper - A funtion that changes
+ * all lowercase letters of a string to uppercase
+ * @p: lowercase letters taken in
+ * Return: uppercase letters
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] <= 'z' && s[i] >= 'a')
+			s[i] = (s[i] - 32);
+		i++;
+	}
+	return (s);
+}
